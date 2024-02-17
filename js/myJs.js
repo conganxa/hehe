affa67a8c3adbccc65419083eb0e11b1eed7a85a
conganxa <126637760+conganxa@users.noreply.github.com>
2024-02-17 15:41:34 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "He luu bà chị!",
+  text2: "Thằng em có điều này muốn hỏi bà chị phải trả lời thật lòng nhaaa.",
+  text3: "bà chị đang nói xạo phải không nào ._.",
+  text4: "Nếu bà chị ko trả lời mà thoát ra tức là nói xạo nha :v",
+  text5: "Nói thật mà???",
+  text6: "Đúng rồi <3",
+  text7: "lí do bà chị nói xạo là gì vậy :vvvv",
+  text8: "Bấm vào đây để gửi <3",
+  text9: "Vì em đẹp try vlllll",
+  text10: "Em biết mà ^^ Ngày xưa đẹp try lắm bây giờ đỡ hơn rồi",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Đến đây rồi thì còn chờ gì nữa mà ko inbox cho thằng em đi nàooo",
   text12: "Okii lunn <3",
 };
 
